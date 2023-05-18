4dcd64c5f0eadf5046372dd04591b6605bdb6174
Karol Wolski <karolwolski04@gmail.com>
2023-05-18 21:34:49 +0200

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
