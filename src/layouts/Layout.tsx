6b2b7dedd1d98f9c19879e7578f93614952c2067
Karol Wolski <karolwolski04@gmail.com>
2023-08-14 01:06:16 +0200

    feat: navigation (#59)

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,9 +1,18 @@
+import Navigation from '../components/Navigation/Navigation'
+
 type Props = {
   children: string | JSX.Element | JSX.Element[]
 }
 
 const Layout = ({ children }: Props) => {
-  return <div className="mx-[1.6rem]">{children}</div>
+  return (
+    <>
+      <Navigation />
+      <div className="max-w-[123.2rem] mx-auto my-[5rem] px-[1.6rem]">
+        {children}
+      </div>
+    </>
+  )
 }
 
 export default Layout
