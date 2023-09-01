7da39f8b446a3868f8b2247e93e059e285157f61
Karol Wolski <karolwolski04@gmail.com>
2023-09-02 00:27:48 +0200

    feat: display set name in course component - integration with BE

--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -1,20 +1,30 @@
 import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
 import SectionWithSet from '../components/SectionWithSets/SectionWithSet'
 import StudyModes from '../components/StudyModes/StudyModes'
 import Layout from '../layouts/Layout'
 import { Sets } from '../ts/types/Sets'
-import { GetSets } from '../api/cardsApi'
+import { GetSetName, GetSets } from '../api/cardsApi'
 
 const CoursePage = () => {
-  const { data = [] } = useQuery<Sets[]>({
+  const { id } = useParams()
+  const { data: name } = useQuery<string>({
+    queryKey: ['setName', { id }],
+    queryFn: () => GetSetName(id),
+  })
+
+  const { data: sets = [] } = useQuery<Sets[]>({
     queryKey: ['sets', { limit: 2 }],
     queryFn: () => GetSets(2),
   })
 
   return (
     <Layout>
-      <StudyModes href="" css="mb-[5rem]" />
-      <SectionWithSet title="Your sets" array={data} />
+      <div className="max-w-[61rem] mx-auto">
+        <h2 className="text-[1.6rem] mb-[1.6rem] font-bold">{name}</h2>
+        <StudyModes href="" css="mb-[5rem]" />
+      </div>
+      <SectionWithSet title="Your sets" array={sets} />
     </Layout>
   )
 }
