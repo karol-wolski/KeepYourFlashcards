a8cd680d58ba032f2f026bae33fcc581d025b991
Karol Wolski <karolwolski04@gmail.com>
2023-09-12 13:14:08 +0200

    fix(useLogin): window location reload after logged in

--- src/hooks/useLogIn.tsx
+++ src/hooks/useLogIn.tsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { useMutation } from '@tanstack/react-query'
 import { loginUserFn } from '../api/authApi'
 import { AuthContext, IAuthContext } from '../context/AuthContext'
@@ -7,14 +6,13 @@ import { HandleMsg } from '../ts/types/HandleMsg'
 
 const useLogIn = (handleError: HandleMsg) => {
   const { setIsLoggedIn } = useContext(AuthContext) as IAuthContext
-  const navigate = useNavigate()
   return useMutation({
     mutationFn: loginUserFn,
     onSuccess: (data) => {
       localStorage.setItem('token', data.data.token)
       handleError('')
       setIsLoggedIn((state: boolean) => !state)
-      return navigate('/profile')
+      window.location.reload()
     },
     onError: ({ response }) => {
       handleError(response.data.message)
