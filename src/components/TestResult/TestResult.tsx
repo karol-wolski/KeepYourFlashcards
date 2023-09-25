81c8dbf0e09d840cb5b9d01170e01466012617a8
Karol Wolski <karolwolski04@gmail.com>
2023-09-25 14:07:38 +0200

    fix(TestResult): display section title only once (#129)

--- src/components/TestResult/TestResult.tsx
+++ src/components/TestResult/TestResult.tsx
@@ -21,28 +21,30 @@ const TestResult = ({
       </p>
 
       {failedAndswerArray.length ? (
-        failedAndswerArray.map((elem) => {
-          return (
-            <div className="mb-[0.8rem]" key={elem.id}>
-              <h3 className="text-[1.8rem] mb-[1.6rem]">
-                List of question with an incorrect answer:
-              </h3>
-              <p className="text-[1.4rem]">{elem.question}</p>
-              <p className="text-[1.4rem]">
-                Your answer:{' '}
-                <span className="text-danger text-[1.4rem] font-bold">
-                  {elem.yourAnswer}
-                </span>
-              </p>
-              <p className="text-[1.4rem]">
-                Correct answer:{' '}
-                <span className="text-success text-[1.4rem] font-bold">
-                  {elem.correctAnswer}
-                </span>
-              </p>
-            </div>
-          )
-        })
+        <>
+          <h3 className="text-[1.8rem] mb-[1.6rem]">
+            List of question with an incorrect answer:
+          </h3>
+          {failedAndswerArray.map((elem) => {
+            return (
+              <div className="mb-[0.8rem]">
+                <p className="text-[1.4rem]">{elem.question}</p>
+                <p className="text-[1.4rem]">
+                  Your answer:{' '}
+                  <span className="text-danger text-[1.4rem] font-bold">
+                    {elem.yourAnswer}
+                  </span>
+                </p>
+                <p className="text-[1.4rem]">
+                  Correct answer:{' '}
+                  <span className="text-success text-[1.4rem] font-bold">
+                    {elem.correctAnswer}
+                  </span>
+                </p>
+              </div>
+            )
+          })}
+        </>
       ) : (
         <p className="text-[1.6rem]">
           Good job. All your answers are correct. Keep it up :)
